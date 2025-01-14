ccd1e8c929edfc108f238221b259b5ddaf2edd36
VaidikKhurana <vaidikkhurana@gmail.com>
2025-01-14 23:01:40 +0530

    made the text white

--- app/easter/page.js
+++ app/easter/page.js
@@ -7,7 +7,7 @@ import { GeistSans } from "geist/font/sans";
 export default function page() {
   return (
    <div className={GeistSans.className}>
-      <div className="flex justify-center min-h-screen bg-black items-center font-medium text-center flex-col">
+      <div className="flex justify-center min-h-screen bg-black items-center font-medium text-white text-center flex-col">
         <h1 className="text-white text-6xl w-[70vw]">Tell more about yourself with a simple <span className="bg-yellow-600">blynkard</span> </h1>
         <div className="w-[30vw] border-gray-700 border-2 h-[25vw] px-[7vw] py-[1.5vw] rounded-3xl mt-[3vw] flex-col justify-center items-center">
           <img src="./pfp.jpg" className="rounded-full w-[9vw]"></img>
